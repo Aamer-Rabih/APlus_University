1ff6aeade22c30f3e601d79cb9924f21e3cca564
Aamer-Rabih <Aamer.rabih@yahoo.com>
2019-03-31 22:41:54 +0300

    Initial commit

---
+++ app/src/main/java/com/raw/pc/aplusuniversity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.raw.pc.aplusuniversity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
